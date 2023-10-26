405e7392aba57b17bcf8ff9d226e4127dd98264a
EA - Bernardo Xavier Raposo <bernardo_raposo@educadventista.org>
2023-10-26 14:54:35 -0300

    recebendo parametro

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id' , (requisicao, resposta) =>{
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuario ${id}`)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/' ,(requisicao, resposta)=> {
      resposta.sendfile(`${caminhoBase}/index.html`)  
 })
