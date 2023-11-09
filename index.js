ff38a2c71023e838f612f0cdec7460f5378d43b0
zeyti12a <daviraposo124@gmail.com>
2023-11-09 15:09:47 -0300

    recebedno informaçoes do usuario

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+
+const usuario =   {
+    name: "Bernardo Raposo" ,
+    email: "daviraposo124@gmail.com" ,
+    senha: 1234
+}
+
+app.post('/cadastrar/salvar',(requisicao, resposta) => {
+   const nome = requisicao.body.nome
+   const email = requisicao.body.email
+   const senha = requisicao.body.senha
+
+   console.log (`o email do usuario é: ${email}`)
+   
+})
+
+app.get('/cadastrar' , (requisicao, resposta) => {
+    resposta.sendfile(`${caminhoBase}/cadastro.html`)
+})
+
 app.get('/usuarios/:id' , (requisicao, resposta) =>{
     const id = requisicao.params.id
 
